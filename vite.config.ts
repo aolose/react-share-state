e578d1f9faed375bb9dbc66bae12ec0f52db2c89
tom <i@aolose.cc>
2023-03-23 19:14:47 +0800

    fix package.json

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,19 @@
 import { defineConfig } from 'vite'
 import {resolve} from 'path'
+import dts from "vite-plugin-dts";
+
 export default defineConfig({
+  plugins: [
+    dts({
+      insertTypesEntry: true,
+    }),
+  ],
   build:{
     lib:{
       entry:resolve(__dirname, 'src/index.ts'),
       name:'reactShareState',
-      fileName:'index'
+      fileName:'index',
+      formats:['es','cjs','umd']
     },
     rollupOptions:{
       external:['react']
